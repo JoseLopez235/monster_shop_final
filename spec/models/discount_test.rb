d535c75c3909d1a04ded666bb708e044c28ce370
Jose Lopez <joselopez11394@gmail.com>
2020-11-08 18:16:05 -0500

    adds test to model and cart show page that allows the discount to be added

--- spec/models/discount_test.rb
+++ spec/models/discount_test.rb
@@ -9,4 +9,20 @@ RSpec.describe Discount do
     it {should validate_presence_of :discount}
     it {should validate_presence_of :quantity}
   end
+
+  describe "Instance Methods" do
+    before :each do
+      @megan = Merchant.create!(name: 'Megans Marmalades', address: '123 Main St', city: 'Denver', state: 'CO', zip: 80218)
+      @m_user = @megan.users.create(name: 'Megan', address: '123 Main St', city: 'Denver', state: 'CO', zip: 80218, email: 'megan@example.com', password: 'test', role: 1)
+      @ogre = @megan.items.create!(name: 'Ogre', description: "I'm an Ogre!", price: 20.25, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 50 )
+      @giant = @megan.items.create!(name: 'Giant', description: "I'm a Giant!", price: 50, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 30 )
+
+      @order_1 = @m_user.orders.create!(status: "pending")
+      @full_order = @order_1.order_items.create!(item: @ogre, price: @ogre.price, quantity: 20)
+
+      @discount = @megan.discounts.create!(discount: 5, quantity: 10)
+      @discount = @megan.discounts.create!(discount: 10, quantity: 50)
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@m_user)
+    end
+  end
 end
